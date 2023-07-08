0f4117c4374cdc493f17532566ce592dba275ad0
mamdinho <mamdinho777@gmail.com>
2023-07-08 12:19:47 -0400

    Bug loading black screen

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,17 @@ import img5 from '../public/epn.jpeg'
 import ContactPage from '../components/contactform'
 import Link from 'next/link'
 
+
 export default function Home() {
   const { sticky, stickyRef } = useSticky();
+  const CollapsibleText = ({ text, maxLines = 3 }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const style = isExpanded ? {} : { 
+      display: "-webkit-box", 
+      WebkitLineClamp: maxLines, 
+      WebkitBoxOrient: "vertical", 
+      overflow: "hidden" 
+    };
   return (
     <div className={styles.container}>
       <Head>
@@ -23,9 +32,9 @@ export default function Home() {
       <div className={styles.landingpage}>
         <h2 className={styles.shine} id='home'>Dinho Fitness</h2>
         <div className={styles.bgcover}>
-        </div> 
-        <Image src={img3} alt='' className={styles.landingbg}/>
-        <ScrollArrow/>
+        </div>
+        <Image src={img3} alt='' className={styles.landingbg} />
+        <ScrollArrow />
       </div>
       <div className={styles.main}>
         <div className={styles.navcontainer}>
@@ -42,48 +51,62 @@ export default function Home() {
             <h2 className={styles.subheader} id='about'>About Me</h2>
             <div className={styles.aboutwrapper}>
               <div className={styles.aboutimgwrapper}>
-                <Image src={img3} alt='' className={styles.aboutimg}/>
+                <Image src={img3} alt='' className={styles.aboutimg} />
               </div>
               <p className={styles.aboutme}>
-                My name is Mohammed, I'm a Fitness Enthusiast and I've been training for over 6 years now. I competed in a couple Men's Physique competitions, and my passion and drive for fitness has led me to want to help others achieve their goals as well. <br></br> As a personal trainer, I specialize in creating custom meal and fitness plans that are accessible to everyone, because I believe guidance shouldn't be limited by cost.
-                I have experience training clients remotely from around the world, including Canada, Tanzania, Malaysia, and Oman. My goal is to provide personalized guidance and support to help clients achieve their fitness goals no matter where they are located. Whether you're a beginner or an experienced athlete, I'm committed to helping you reach your full potential and achieve the body of your dreams.
+                My name is Mohammed, I'm a Fitness Enthusiast and I've been training for over 6 years now. I competed in two Men's Physique competitions, and my passion and drive for fitness has led me to want to help others achieve their goals as well. <br></br> As a trainer, I specialize in creating custom meal and fitness plans that are accessible to everyone, because I believe guidance shouldn't be limited by cost.
+                I have experience training clients remotely from around the world ! My goal is to provide guidance and support to help clients achieve their fitness goals no matter where they are located. Whether you're a beginner or an experienced athlete, I'm committed to helping you reach your full potential and achieve the body of your dreams.
               </p>
-              </div>
-              <div className={styles.aboutbtnwrapper}>
-                <Link className={styles.aboutbtn} href='/FitnessJourney'>Check out my fitness journey</Link>
-              </div>
+            </div>
+            <div className={styles.aboutbtnwrapper}>
+              <Link className={styles.aboutbtn} href='/FitnessJourney'>Check out my fitness journey</Link>
+            </div>
           </div>
           <div className={styles.section}>
             <h2 className={styles.subheader} id='shop'>Shop</h2>
             <div className={styles.cards}>
               <div className={styles.card}>
                 <div className={styles.imgwrapper}>
-                    <Image className={styles.prodimg} src={img6} alt=''/>
-                    <p className={styles.pricetag}>$10.00</p>
+                  <Image className={styles.prodimg} src={img6} alt='' />
+                  <p className={styles.pricetag}>$10.00</p>
                 </div>
                 <p className={styles.proddescription}>BUILD Cookbook</p>
+                <p className={styles.proddetail}>The BUILD Cookbook consists of calorie dense healthy meals that are simple to make, and taste great!</p>
                 <div className={styles.btnwrapper}>
                   <a href='https://buy.stripe.com/28odRv5yP0sEbVm001' className={styles.buybtn}>Buy now</a>
                 </div>
               </div>
               <div className={styles.card}>
                 <div className={styles.imgwrapper}>
-                    <Image className={styles.prodimg} src={img2} alt=''/>
-                    <p className={styles.pricetag}>$10.00</p>
+                  <Image className={styles.prodimg} src={img2} alt='' />
+                  <p className={styles.pricetag}>$10.00</p>
                 </div>
                 <p className={styles.proddescription}>SHRED Cookbook</p>
+                <p className={styles.proddetail}>The SHRED Cookbook consists of less calorie dense healthy meals which are perfect when you are trying to lose fat.</p>
                 <div className={styles.btnwrapper}>
                   <a href='https://buy.stripe.com/dR66p39P5b7i9NefYY' className={styles.buybtn}>Buy Now</a>
                 </div>
               </div>
               <div className={styles.card}>
                 <div className={styles.imgwrapper}>
-                    <Image className={styles.prodimg} src={img1} alt=''/>
-                    <p className={styles.pricetag}>$85.00/month</p>
+                  <Image className={styles.prodimg} src={img1} alt='' />
+                  <p className={styles.pricetag}>$85.00/month</p>
                 </div>
                 <p className={styles.proddescription}>Online Personal Training Program</p>
+                <p className={styles.proddetail}>One on one training program with me where I'll go over your check ins with you every weekend and track your progress. You will also have direct contact with my personal number and can ask me literally anything.</p>
                 <div className={styles.btnwrapper}>
                   <a href='https://buy.stripe.com/6oE00F3qHb7i2kMcMO' className={styles.buybtn}>Buy now</a>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.imgwrapper}>
+                  <Image className={styles.prodimg} src={img1} alt='' />
+                  <p className={styles.pricetag}>FREE</p>
+                </div>
+                <p className={styles.proddescription}>THE STUBBORN FAT</p>
+                <p className={styles.proddetail}>This is an E-book that I personally enjoyed reading. It gives a ton of insight as to why you might not be loosing that stubborn belly fat. </p>
+                <div className={styles.btnwrapper}>
+                  <a href='https://dinhofitness-public.s3.amazonaws.com/E-books/The+Stubborn+Fat+Solution+-+Flatten+Your+Abs.pdf' download className={styles.buybtn}>Download</a>
                 </div>
               </div>
             </div>
@@ -91,8 +114,8 @@ export default function Home() {
             <div className={styles.cards}>
               <div className={styles.card}>
                 <div className={styles.imgwrapper}>
-                    <Image className={styles.prodimg} src={img4} alt=''/>
-                    <p className={styles.pricetag}>25% OFF</p>
+                  <Image className={styles.prodimg} src={img4} alt='' />
+                  <p className={styles.pricetag}>25% OFF</p>
                 </div>
                 <p className={styles.proddescription}>Get 25% off jewelry using code "MAMDISAVE25%" **Apply code at Checkout</p>
                 <div className={styles.btnwrapper}>
@@ -101,8 +124,8 @@ export default function Home() {
               </div>
               <div className={styles.card}>
                 <div className={styles.imgwrapper}>
-                    <Image className={styles.prodimg} src={img5} alt=''/>
-                    <p className={styles.pricetag}>10% OFF</p>
+                  <Image className={styles.prodimg} src={img5} alt='' />
+                  <p className={styles.pricetag}>10% OFF</p>
                 </div>
                 <p className={styles.proddescription}>Get 10% off Nutritional Supplements using code "MFALAHY" **Apply code at Checkout</p>
                 <div className={styles.btnwrapper}>
@@ -114,7 +137,7 @@ export default function Home() {
           <div className={styles.section}>
             <h2 className={styles.subheader} id='contact'>Leave me a message</h2>
             <div className={styles.contactsection}>
-              <ContactPage/>
+              <ContactPage />
             </div>
           </div>
         </div>
@@ -123,8 +146,9 @@ export default function Home() {
       <footer className={styles.footer}>
         <div className={styles.copyrightline}>
           <p className={styles.copyright}><span className={styles.footerlogo}>DINHO FITNESS</span> © 2023</p>
-      </div>
+        </div>
       </footer>
     </div>
   )
+}
 }
