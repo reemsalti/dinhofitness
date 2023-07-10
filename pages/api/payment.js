93b9b7f882d937c50e73f4fe5df2a230fb9cd874
mamdinho <mamdinho777@gmail.com>
2023-07-10 12:27:32 -0400

    after payment process

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -1,5 +1,17 @@
 import { Stripe } from 'stripe';
-import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
+import AWS from 'aws-sdk';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION
+});
+const s3 = new AWS.S3();
+const urlExpirationTime = 60 * 60; 
+
 
 const handler = async (req, res) => {
   const stripe = new Stripe(process.env.STRIPE_KEY);
@@ -7,36 +19,31 @@ const handler = async (req, res) => {
 
   if (event.type === 'checkout.session.completed') {
     const session = event.data.object;
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.MY_EMAIL,
-        pass: process.env.PASSWORD
-      }
-    });
 
-    const mailOptions = {
-      from: process.env.MY_EMAIL,
+    const params = {
+      Bucket: 'dinhofitness-public',
+      Key: 'MyCookBook.pdf', 
+      Expires: urlExpirationTime
+    };
+    const signedUrl = s3.getSignedUrl('getObject', params);
+    console.log(signedUrl);  // Outputs the signed URL
+    const msg = {
       to: session.customer_email,
+      from: process.env.MY_EMAIL,
       subject: 'Your Cookbook',
-      text: 'Thank you for your purchase! Please find your cookbook attached.',
-      attachments: [
-        {
-          filename: 'MyCookBook.pdf',
-          path: '../public/MyCookBook.pdf',
-          contentType: 'application/pdf'
-        }
-      ]
+      text: `Welcome to the dinhoFitness Fam !!! Thank you for your purchase :) I hope you find the recipes and macros appealing. You can download your cookbook here: ${signedUrl}`
     };
 
-    transporter.sendMail(mailOptions, function(err, info){
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error sending email' });
-      } else {
-        res.json({ received: true });
+    try {
+      await sgMail.send(msg);
+      res.json({ received: true });
+    } catch (error) {
+      console.error(error);
+      if (error.response) {
+        console.error(error.response.body)
       }
-    });
+      res.status(500).json({ error: 'Error sending email' });
+    }
   }
 }
 
